e39aa341c8908a893cae0132f0925de6078a8804
Philip Ensomo <philipensomo@gmail.com>
2016-11-27 09:40:06 +0800

    Initial commit

---
+++ src/main/java/ph/com/accountingsystem/AccountingSystemApplication.java
@@ -0,0 +1,12 @@
+package ph.com.accountingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingSystemApplication.class, args);
+	}
+}
